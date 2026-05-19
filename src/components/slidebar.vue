d8130a29902660fe2763abd5a50dc600d6917b3a
Anish Upreti <uanish767@gmail.com>
2026-05-19 19:09:40 +0545

    made change in components

--- src/components/slidebar.vue
+++ src/components/slidebar.vue
@@ -1,19 +1,90 @@
 <template>
-  <div class="py-8 overflow-hidden bg-gradient-to-br from-[#fbfcfe] to-[#fdf2fb]">
-    <div class="flex w-max gap-32 animate-scroll hover:[animation-play-state:paused]">
+  <div
+    class="py-8 overflow-hidden bg-gradient-to-br from-[#fbfcfe] to-[#fdf2fb]"
+  >
+    <div
+      class="flex w-max gap-32 animate-scroll hover:[animation-play-state:paused]"
+    >
       <!-- First set -->
-      <img src="/esewa.png" alt="Esewa" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/cg-learning.png" alt="CG Learning" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/annapurna-media.png" alt="Annapurna Media" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/news-karkhana.B12V9KJQ.png" alt="News Karkhana" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/brij-cement.png" alt="Brij Cement" class="h-[50px] w-auto object-contain shrink-0" />
+      <img
+        src="/esewa.png"
+        alt="Esewa"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/cg-learning.png"
+        alt="CG Learning"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/annapurna-media.png"
+        alt="Annapurna Media"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/news-karkhana.B12V9KJQ.png"
+        alt="News Karkhana"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/brij-cement.png"
+        alt="Brij Cement"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
       <!-- Duplicate for seamless loop -->
-      <img src="/esewa.png" alt="Esewa" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/cg-learning.png" alt="CG Learning" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/annapurna-media.png" alt="Annapurna Media" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/news-karkhana.B12V9KJQ.png" alt="News Karkhana" class="h-[50px] w-auto object-contain shrink-0" />
-      <img src="/brij-cement.png" alt="Brij Cement" class="h-[50px] w-auto object-contain shrink-0" />
+      <img
+        src="/esewa.png"
+        alt="Esewa"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/cg-learning.png"
+        alt="CG Learning"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/annapurna-media.png"
+        alt="Annapurna Media"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/news-karkhana.B12V9KJQ.png"
+        alt="News Karkhana"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
+      <img
+        src="/brij-cement.png"
+        alt="Brij Cement"
+        class="h-[50px] w-auto object-contain shrink-0"
+      />
     </div>
   </div>
-</template>
+  <div class="px-4 md:px-8 lg:px-20 py-16 md:py-24 overflow-hidden">
+  <div
+    class="max-w-[1280px] w-full mx-auto min-h-[320px] flex items-center justify-center rounded-[40px] bg-cover bg-center bg-no-repeat"
+    style="background-image: url('https://sociair.com/_nuxt/readyToSimplify.Cq6l0h_u.svg');"
+  >
+    <div class="text-center px-6">
+      
+      <!-- Heading -->
+      <h2
+        class="text-[32px] md:text-[52px] font-bold leading-tight text-[#2E353E]"
+      >
+        Ready To Simplify Your
+        <span class="text-[#E77619]">Business?</span>
+      </h2>
 
+      <!-- Button -->
+      <div class="mt-10">
+        <a
+          href="/register"
+          class="inline-flex items-center justify-center bg-[#2374E1] hover:bg-[#1E63C7] text-white font-semibold text-lg px-10 py-4 rounded-full transition duration-300"
+        >
+          Start a Free Trial
+        </a>
+      </div>
+
+    </div>
+  </div>
+</div>
+</template>
